a1a2f123db1ea3c004f6307ebe2ca738657c6b26
trishas18 <trishasudhakar2@gmail.com>
2023-10-10 16:29:18 +0530

    mern projrct

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('connected..!!');
+}).catch((err) => {
+console.log('err..');
+});
+const app=express();
+app.listen(3001, ()=>{
+console.log('server is running on port  3001');
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Header from './components/Header';
+
+export default function App() {
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/sign-in" element={<SignIn/>} />
+    <Route path="/sign-up" element={<SignOut/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>;
+}
