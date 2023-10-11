9dfcf9399d05f4e9f81adc4ab784d3a1e280ac32
trishas18 <trishasudhakar2@gmail.com>
2023-10-11 19:40:56 +0530

    signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(() =>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected..!!');
 }).catch((err) => {
-console.log('err..');
+    console.log('err..');
 });
-const app=express();
-app.listen(3001, ()=>{
-console.log('server is running on port  3001');
+const app = express();
+app.use(express.json());
+app.listen(3001, () => {
+    console.log('server is running on port  3001');
+});
+
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
+    username: {
+        type: String,
         required: true,
         unique: true,
 
     },
-   email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
         unique: true,
 
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true,
     }
 
-},{timestamps:true});
-const User = mongoose.model('User',userSchema);
+}, { timestamps: true });
+const User = mongoose.model('User', userSchema);
 export default User;
