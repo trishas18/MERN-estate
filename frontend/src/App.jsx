1b2eee99f3ec7d55ea70010d4a33f76cc2d8325c
trishas18 <trishasudhakar2@gmail.com>
2023-10-21 17:07:44 +0530

    listing api route,update functionality,image slider,complete listing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import About from './pages/About';
 import Header from './components/Header';
 import PrivateRoute from '../src/components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 export default function App() {
   return <BrowserRouter>
@@ -17,10 +18,14 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
+      <Route path='/listing/:listingId' element={<Listing />} />
       <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/create-listing' element={<CreateListing />} />
-     
+        <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         
       </Route>
     </Routes>
